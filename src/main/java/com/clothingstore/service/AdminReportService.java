d848a94ce5a3379e3a2c27a3308f1c496a99267b
DANG VAN TAN <161315905+tank22BKU@users.noreply.github.com>
2025-12-09 16:01:43 +0700

    Update Report: Fix columns in error values + Remove unusing log

--- src/main/java/com/clothingstore/service/AdminReportService.java
+++ src/main/java/com/clothingstore/service/AdminReportService.java
@@ -52,7 +52,6 @@ public class AdminReportService {
             List<ProductSalesReportDto> reports = new ArrayList<>();
 
             for (Object[] row : results) {
-                log.info("Row " + row.toString());
                 BigDecimal totalRenevue = row[4] != null ? (BigDecimal) row[4] : BigDecimal.ZERO;
                 BigDecimal averagePrice = row[2] != null ? (BigDecimal) row[2] : BigDecimal.ZERO;
                 Integer totalQuantitySold = convertToInteger(row[3]);
